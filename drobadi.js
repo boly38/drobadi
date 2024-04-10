d039c75f2388117105f9494e3e864d21e2c9a827
Brice Vandeputte <brice.vandeputte@orange.com>
2024-04-10 08:57:38 +0200

    Fix #35 require, update setup help, fix backup response

--- drobadi.js
+++ drobadi.js
@@ -1,4 +1,4 @@
-const {DCommand} = require('./lib/drobadi.js');
+import DCommand from './lib/DCommand.js';
 // take first command line argument
 const action = process.argv.slice(2)[0];
 const args = process.argv.slice(3);
